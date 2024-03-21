38966412f1f5e78372efade2402446f565bdb575
Carter Albers <caal2213@colorado.edu>
2024-03-21 12:09:10 -0600

    wa10

--- js/wa10.js
+++ js/wa10.js
@@ -2,11 +2,11 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then  :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"]
-const insertY = ["the soup kitchen","Disneyland","the White House"]
-const insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"]
+const insertX = ["The boy", "Ernie the Hog Snatcher", "Poppins"]
+const insertY = ["the ghetto","Cabo","prison"]
+const insertZ = ["Pranced around","Wasen't a microdose","Started spinning in the rain"]
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
